4ac2dd68410485f0b10c045a3016ae03b3dd028f
Ivan Malison <IvanMalison@gmail.com>
2023-04-13 17:05:21 -0600

    Try to be a bit more permissive with player mmr estimate scoring

--- rocket_league_mmr_prediction/filter.py
+++ rocket_league_mmr_prediction/filter.py
@@ -20,6 +20,11 @@ def default_estimate_calculator(date, mmr_history_by_season):
     ).get_mmr(date)
 
 
+class MetaScoreTooLow(Exception):
+
+    pass
+
+
 class MMREstimateQualityFilter:
 
     def __init__(
@@ -33,7 +38,7 @@ class MMREstimateQualityFilter:
         self._score_game_count = score_game_count
         self._meta_score = meta_score
 
-    def score_replay_meta(self, meta: _replay_meta.ReplayMeta):
+    def score_replay_meta(self, meta: _replay_meta.ReplayMeta, abort_score=0.0):
         game_date = meta.datetime.date()
         estimates = []
         scores = []
@@ -41,6 +46,8 @@ class MMREstimateQualityFilter:
             estimate, score = self.score_player_mmr_estimate(player, game_date)
             estimates.append((player.tracker_suffix, estimate))
             scores.append(score)
+            # if estimate is None or self._meta_score(scores) < abort_score:
+            #     raise MetaScoreTooLow()
 
         meta_score = self._meta_score(scores)
         return meta_score, estimates, scores
@@ -50,15 +57,56 @@ class MMREstimateQualityFilter:
             playlist='Ranked Doubles 2v2'
     ):
         player_data = self._get_player_data(player)
+
+        if player_data is None:
+            return 0, 0.0
+
         season_at_date = mmr.get_season_for_date(date, season_dates=self._season_dates)
+
+        try:
+            playlist_mmr_history = player_data['mmr_history'][playlist]
+        except Exception:
+            playlist_mmr_history = []
+
         history_by_season = mmr.split_mmr_history_into_seasons(
-            player_data['mmr_history'].get(playlist, []),
+            playlist_mmr_history,
             season_dates=self._season_dates
         )
-        season_to_data_points = dict(history_by_season)
-        estimate = self._estimate_caculator(date, history_by_season)
 
+        stats = mmr.calculate_all_season_statistics(history_by_season)
+        history_estimate, score = self._calculate_season_history_mmr_estimate(
+            date, history_by_season, stats
+        )
+
+        all_mmrs = [mmr for _, mmr in playlist_mmr_history]
+
+        if not all_mmrs:
+            return (0.0, 0.0)
+
+        all_time_max = max(all_mmrs)
+        if history_estimate is None or score < .15:
+            if len(playlist_mmr_history) > 0 and player_data['stats']['wins'] > 150:
+                all_history_median = np.median([mmr for _, mmr in playlist_mmr_history])
+                if all_time_max - all_history_median < 200:
+                    return all_history_median, .15
+
+        return (history_estimate, score)
+
+    def meta_download_filter(self, replay_meta):
+        try:
+            score, _, _ = self.score_replay_meta(replay_meta)
+            return score > 0
+        except MetaScoreTooLow:
+            return False
+
+    def _calculate_season_history_mmr_estimate(self, date, history_by_season, stats):
+        calculator = mmr.SeasonBasedPolyFitMMRCalculator(
+            history_by_season,
+            season_dates=self._season_dates,
+            stats=stats,
+        )
+        season_at_date = mmr.get_season_for_date(date, season_dates=self._season_dates)
+        season_to_data_points = dict(history_by_season)
         data_points_count = len(season_to_data_points.get(season_at_date, []))
         score = self._score_game_count(data_points_count)
-
-        return (estimate, score)
+        return calculator.get_mmr(date), score
