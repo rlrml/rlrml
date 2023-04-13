6ba4b19681fada58bc4de3e7dbe6d8166abca618
Ivan Malison <IvanMalison@gmail.com>
2023-04-13 17:08:08 -0600

    Add some sensible default filtering to replay_downloader

--- rocket_league_mmr_prediction/download/console.py
+++ rocket_league_mmr_prediction/download/console.py
@@ -1,15 +1,26 @@
 """Defines a command line interface to the replay fetcher defined in this package."""
 import argparse
+import coloredlogs
 import os
 import pprint
 import tqdm
-from .replay_downloader import ReplayDownloader
+import logging
+
+from . import replay_downloader
 from .progress import BarProgressHandler
+
+from ..filter import MMREstimateQualityFilter
+from .. import console
+from .._replay_meta import ReplayMeta
+from .. import player_cache as pc
+from .. import tracker_network
+from .. import util
+from .. import _replay_meta
 
 
 def run():
     """Entry point to the command line interface to ballchasing_replay_fetcher."""
-    parser = argparse.ArgumentParser()
+    parser = console._add_rlrml_args(argparse.ArgumentParser())
 
     default_path = os.path.join(os.getcwd(), "replays")
     parser.add_argument(
@@ -27,7 +38,68 @@ def run():
         '--query', '-q', action='append', nargs=2, metavar=('PARAM', 'VALUE'),
         help="Add a query parameter"
     )
+    parser.add_argument(
+        '--logs', type=bool, default=True
+    )
+    parser.add_argument(
+        '--all-replays-dir', type=str,
+        help="The directory to use for replay filtering", default=None
+    )
     args = parser.parse_args()
+    all_replays_dir = args.all_replays_dir or args.path
+
+    existing_replay_uuids = set(
+        uuid for uuid, _ in util.get_replay_uuids_in_directory(all_replays_dir)
+    )
+
+    player_cache = pc.PlayerCache(str(args.player_cache))
+
+    def replay_exists(uuid):
+        return uuid in existing_replay_uuids
+
+    async def check_for_cached_error(_, replay_meta):
+        try:
+            meta = _replay_meta.ReplayMeta.from_ballchasing_game(replay_meta)
+            should_enqueue = (not any(bool(player_cache.has_error(player)) for player in meta.player_order))
+            if not should_enqueue:
+                replay_downloader.logger.warn(f"Filtering due to error player {replay_meta}")
+            return (
+                should_enqueue,
+                replay_meta
+            )
+        except Exception as e:
+            import ipdb; ipdb.set_trace()
+            print(f"filtering error {e}")
+            return True
+
+    task_filter = replay_downloader.compose_filters(
+        replay_downloader.require_at_least_one_non_null_mmr,
+        replay_downloader.build_filter_existing(replay_exists),
+        check_for_cached_error,
+    )
+
+    if args.logs:
+        coloredlogs.install(level='INFO', logger=replay_downloader.logger)
+        replay_downloader.logger.setLevel(logging.INFO)
+
+    # player_cache = pc.PlayerCache(str(args.player_cache))
+
+    # cached_player_get = pc.CachedGetPlayerData(
+    #     player_cache, tracker_network.get_player_data_with_429_retry()
+    # ).get_player_data
+
+    # mmr_filter = MMREstimateQualityFilter(cached_player_get)
+
+    # async def task_filter(session, replay_meta):
+    #     should_continue, replay_meta = await require_at_least_one_non_null_mmr(
+    #         session, replay_meta
+    #     )
+    #     if not should_continue:
+    #         return should_continue, replay_meta
+
+    #     return mmr_filter.meta_download_filter(
+    #         ReplayMeta.from_ballchasing_game(replay_meta)
+    #     ), replay_meta
 
     query_params = dict(args.query)
 
@@ -39,9 +111,10 @@ def run():
     )
 
     with tqdm.tqdm(total=args.count) as pbar:
-        ReplayDownloader(
-            args.auth_token, download_path=args.path, download_count=args.count,
+        replay_downloader.ReplayDownloader(
+            args.ballchasing_token, download_path=args.path, download_count=args.count,
             download_task_count=args.task_count, filter_task_count=args.task_count,
             progress_handler=BarProgressHandler(pbar),
             replay_list_query_params=query_params,
+            replay_filter=task_filter,
         ).start_event_loop_and_run()
--- rocket_league_mmr_prediction/download/replay_downloader.py
+++ rocket_league_mmr_prediction/download/replay_downloader.py
@@ -4,6 +4,8 @@ import asyncio
 import json
 import logging
 import os
+import urllib
+
 from .parallel_downloader import ParallelDownloader, ParallelDownloaderConfig
 from .. import manifest
 
@@ -17,17 +19,46 @@ async def always_download_replay_filter(session, replay_meta) -> (bool, dict):
 
 
 async def require_at_least_one_non_null_mmr(_, replay_meta):
-    mmr_estimates = manifest.get_mmr_data_from_manifest_game(replay_meta)
-    logger.info(f"{mmr_estimates}")
+    """Require that at least one of the mmrs in the replay meta from ballchasing is non-null.
+
+    This is actually important because there are replays on ballchasing.com for
+    which there is no estimate on mmr for any player that will pass through any
+    minimum or maximum rank filter. This filter is a quick and dirty way to
+    ensure that the filter is at least semi effective.
+    """
+    try:
+        mmr_estimates = manifest.get_mmr_data_from_manifest_game(replay_meta)
+    except Exception:
+        return False, replay_meta
     return any(
         value is not None
         for value in mmr_estimates.values()
     ), replay_meta
 
 
+def build_filter_existing(replay_exists):
+    """Filter any tasks for replays that already exist."""
+    async def filter_existing(_, replay_meta):
+        return (not replay_exists(replay_meta['id'])), replay_meta
+    return filter_existing
+
+
+def compose_filters(*filters):
+    """Compose the provided filters."""
+    async def new_filter(session, replay_meta):
+        for next_filter in filters:
+            should_enqueue, replay_meta = await next_filter(session, replay_meta)
+            if not should_enqueue:
+                break
+        return should_enqueue, replay_meta
+    return new_filter
+
+
 def use_replay_id(replay_fetcher, replay_metadata):
     """Use the replay's id as the filename for the replay."""
-    return os.path.join(replay_fetcher.download_path, "{}.replay".format(replay_metadata['id']))
+    return os.path.join(
+        replay_fetcher.download_path, "{}.replay".format(replay_metadata['id'])
+    )
 
 
 class ReplayDownloader(ParallelDownloaderConfig):
@@ -53,6 +84,7 @@ class ReplayDownloader(ParallelDownloaderConfig):
         self.ballchasing_base_uri = ballchasing_base_uri
         self.filepath_setter = filepath_setter
         self.save_tasks_to_manifest = save_tasks_to_manifest
+        self._enqueued_uuids = set()
 
     @property
     def _replay_list_request_query_params_string(self):
@@ -89,6 +121,15 @@ class ReplayDownloader(ParallelDownloaderConfig):
 
     # ParalellDownloaderConfig Implementation:
 
+    def _readd_original_query_parameters(self, url):
+        parsed = urllib.parse.urlparse(url)
+        parsed_params = urllib.parse.parse_qs(parsed.query)
+        new_params = dict(self.replay_list_query_params)
+        new_params['after'] = parsed_params['after'][0]
+        logger.info(new_params)
+        logger.info(url)
+        return parsed._replace(query='').geturl(), new_params
+
     async def get_tasks_and_next_request_from_response(self, session, response_obj):
         """Get the next request task and items to be enqueued from a response."""
         if not response_obj:
@@ -98,7 +139,11 @@ class ReplayDownloader(ParallelDownloaderConfig):
             ), []
         else:
             response = await response_obj.json()
-            return (session.get(response["next"]), response["list"])
+            uri, params = self._readd_original_query_parameters(response["next"])
+            return (
+                session.get(uri, params=params),
+                response["list"]
+            )
 
     def get_filepath(self, task_meta):
         """Return the filepath that should be used to store the download."""
@@ -108,6 +153,14 @@ class ReplayDownloader(ParallelDownloaderConfig):
         """Return the request that should be used to download."""
         return session.get(self._replay_download_uri(task_meta["id"]))
 
-    def get_filter_task(self, session, task_meta):
+    async def get_filter_task(self, session, task_meta):
         """Return a coroutine to filter the provided task."""
-        return self.replay_filter(session, task_meta)
+        uuid = task_meta['id']
+        if uuid in self._enqueued_uuids:
+            logger.warn(f"Attempted enqueue of already enqueued uuid {uuid}")
+            return False, task_meta
+        should_enqueue, task_meta = await self.replay_filter(session, task_meta)
+        if should_enqueue:
+            self._enqueued_uuids.add(uuid)
+
+        return should_enqueue, task_meta
