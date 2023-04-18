701af75783733b60d4c374997e05f142c602779f
Kelley Kelley <mike1555@colorado.edu>
2023-04-18 12:20:09 -0600

    retrieve column headers from replay meta

--- rocket_league_mmr_prediction/console.py
+++ rocket_league_mmr_prediction/console.py
@@ -239,7 +239,7 @@ def convert_game(filepath):
     _add_rlrml_args()
     import boxcars_py
     try:
-        meta, tensor = boxcars_py.get_replay_meta_and_numpy_ndarray(filepath)
+        meta, columns, tensor = boxcars_py.get_replay_meta_and_column_headers_and_numpy_ndarray(filepath)
     except (Exception) as e:
         print(e)
         import ipdb; ipdb.set_trace()
@@ -254,6 +254,7 @@ def convert_game(filepath):
     with open("./saved_tensor.pt", 'rb') as f:
         tensor = torch.load(f)
     from . import _replay_meta
+    #import ipdb; ipdb.set_trace()
     print(_replay_meta.ReplayMeta.from_boxcar_frames_meta(meta))
 
 
